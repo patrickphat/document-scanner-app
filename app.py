8d11844446f77a0dd5af2b88ce1b6559da253c29
Patrick Phat Nguyen <patrickphatng@gmail.com>
2020-01-18 20:52:07 +0700

    new scan site

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template, request, flash, request, redirect, url_for, send_from_directory
+import os
+from flask_scss import Scss
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+INPUT_FILE_NAME = "input.jpg"
+OUTPUT_FILE_NAME = "output.jpg"
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], INPUT_FILE_NAME))
+            os.system("python scan.py -i static/images/input.jpg")
+            return redirect(url_for('uploaded_file',
+                                    filename=OUTPUT_FILE_NAME))
+    return render_template("home.html")
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
